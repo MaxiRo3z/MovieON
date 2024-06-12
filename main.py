d89c5be6f897961f75a94fd3abbdfde3ce62c867
Maxi <maxiromeroez@outlook.com>
2024-06-12 17:50:04 -0300

    Update

--- main.py
+++ main.py
@@ -6,5 +6,14 @@ app= Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/login")
+def login():
+    return render_template("inicio_sesion.html")
+
+@app.route("/register")
+def register():
+    return render_template("registro.html")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
