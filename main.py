1167190125ad629a362993c9b849bfcc53ee081c
MauriBonpland <mauriciobonpland@hotmail.com>
2024-09-03 20:14:42 -0300

    update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Routes import main_bp, auth_bp
 from database.db import session
 
 app = Flask(__name__)
-app.config.from_pyfile('.env')
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'your_secret_key'
